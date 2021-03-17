53180961e1e39de769ea8b1618620dc2ebd98450
Darran Gaskell <darran@systemrate.co.uk>
2021-03-17 07:55:09 +0000

    Added command line parsing for number of rows and columns

--- MazeGenerator/Program.cs
+++ MazeGenerator/Program.cs
@@ -9,13 +9,31 @@ namespace MazeGenerator
 
         public static void Main(string[] args)
         {
-            Grid grid = new Grid(MaxRows, MaxCols);
+            if (args.Length == 2)
+            {
+                if (!int.TryParse(args[0], out int numRows))
+                {
+                    Console.WriteLine($"Error {args[0]} is not a valid number of rows");
+                    return;
+                }
+
+                if (!int.TryParse(args[1], out int numCols))
+                {
+                    Console.WriteLine($"Error {args[1]} is not a valid number of columns");
+                    return;
+                }
+
+                MaxRows = numRows;
+                MaxCols = numCols;
+            }
+
+            Grid grid = new Grid(MaxCols, MaxRows);
 
             Random rnd = new Random();
 
             Console.Clear();
             grid.RecursiveBacktrackingGenerator(grid.Cells[0, 0]);
-            Console.SetCursorPosition(0, MaxRows + 2);
+            Console.SetCursorPosition(0, (MaxRows * 2) + 2);
         }
     }
 }
